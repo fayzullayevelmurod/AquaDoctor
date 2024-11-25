79882b928af523b08ce2809b332008b7fbf04810
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-11-25 10:01:57 +0500

    media_menu the end

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
-let navLink = document.querySelector(".header__nav-link");
-let navLinkClose = document.querySelector(".header__nav-link-close");
-let navLinkBurger = document.querySelector(".header__nav-burger");
+let navMenu = document.querySelector(".media_menu");
+let navClose = document.querySelector(".media_menu_close");
+let navBurger = document.querySelector(".burger");
 
-navLinkBurger.addEventListener('click', () => {
-  navLink.classList.add('active')
-});
+navBurger.addEventListener('click', () => {
+  navMenu.classList.add('active');
+})
 
-navLinkClose.addEventListener('click', () => {
-  navLink.classList.remove('active')
-});
-
-navLink.addEventListener('click', () => {
-  navLink.classList.remove('active')
-});
+navClose.addEventListener('click', () => {
+  navMenu.classList.remove('active');
+})
 
 
 
