f984931aabec78bb8a29518eb23b9d20755f37be
Mavlon4658 <mavlonbe4658>
2024-11-25 14:45:06 +0500

    ready

--- js/main.js
+++ js/main.js
@@ -52,3 +52,89 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+
+const newsSwp = document.querySelectorAll('.news .swp');
+
+if (newsSwp.length) {
+  newsSwp.forEach(el => {
+    const swp = new Swiper(el.querySelector('.swiper'), {
+      slidesPerView: 'auto',
+      spaceBetween: 13,
+      loop: true,
+      navigation: {
+        nextEl: el.querySelector('.swp_btn__next'),
+        prevEl: el.querySelector('.swp_btn__prev'),
+      },
+      pagination: {
+        el: el.querySelector('.swp_pagination'),
+        clickable: true,
+      },
+      breakpoints: {
+        1023: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        }
+      }
+    })
+  })
+}
+
+const newsTabBtn = document.querySelectorAll('.news__tab_head button');
+const newsTabItem = document.querySelectorAll('.news__tab_item');
+
+if (newsTabBtn.length) {
+  newsTabBtn.forEach((btn, btnID) => {
+    btn.onclick = () => {
+      newsTabItem.forEach((item, itemID) => {
+        if (itemID == btnID) {
+          item.classList.add('active');
+        } else {
+          item.classList.remove('active');
+        }
+      })
+      newsTabBtn.forEach(el => {
+        if (btn == el) {
+          el.classList.add('active');
+        } else {
+          el.classList.remove('active');
+        }
+      })
+    }
+  })
+}
+
+const serviceItem = document.querySelectorAll('.service__item');
+if (serviceItem.length) {
+  serviceItem.forEach(el => {
+    const header = el.querySelector('h3');
+    const content = el.querySelector('.service__item_body');
+    
+    if (window.innerWidth <= 1023) {
+      header.addEventListener('click', () => {
+        el.classList.toggle('active');
+        content.style.maxHeight = content.style.maxHeight ? null : content.scrollHeight + 'px';
+      });
+    }
+
+  })
+}
+
+const orderModal = document.querySelector('.order-modal');
+const orderModalOpen = document.querySelector('.order-modal__open');
+const orderModalClose = document.querySelector('.order-modal__close');
+const orderModalBg = document.querySelector('.order-modal__bg');
+
+if (orderModalOpen) {
+  orderModalOpen.onclick = () => {
+    orderModal.classList.add('active');
+  }
+
+  orderModalClose.onclick = () => {
+    orderModal.classList.remove('active');
+  }
+
+  orderModalBg.onclick = () => {
+    orderModal.classList.remove('active');
+  }
+}
