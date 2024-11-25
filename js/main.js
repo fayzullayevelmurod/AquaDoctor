988daf9bdae5ac95680e479cf288fa1d827577dd
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-11-25 10:51:49 +0500

    partner slider the end

--- js/main.js
+++ js/main.js
@@ -12,25 +12,29 @@ navClose.addEventListener('click', () => {
 
 
 
-var swiper = new Swiper(".treatmentsSwiper", {
-  slidesPerView: 1.10,
-  spaceBetween: 28,
+var swiper = new Swiper(".partnerSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".partner-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: ".partner-button-next",
+    prevEl: ".partner-button-prev",
+  },
   breakpoints: {
-    640: {
-      slidesPerView: 2.2,
-      spaceBetween: 28,
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
     993: {
-      slidesPerView: 3.2,
-      spaceBetween: 28,
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
     1200: {
-      slidesPerView: 3.6,
-      spaceBetween: 40,
+      slidesPerView: 3,
+      spaceBetween: 20,
     },
   },
 });
