deff61dde15305844f9c060c9e0df231aaaeb921
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-11-25 14:16:08 +0500

    product section the end

--- js/main.js
+++ js/main.js
@@ -56,3 +56,22 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// const container = document.querySelector('.product .container');
+// document.querySelector('.product .slider').addEventListener('input', (e) => {
+//   container.style.setProperty('--position', `${e.target.value}%`);
+// })
+
+
+var divisor = document.getElementById("divisor"),
+    handle = document.getElementById("handle"),
+    slider = document.getElementById("slider");
+
+function moveDivisor() {
+  handle.style.left = slider.value+"%";
+	divisor.style.width = slider.value+"%";
+}
+
+window.onload = function() {
+	moveDivisor();
+};
