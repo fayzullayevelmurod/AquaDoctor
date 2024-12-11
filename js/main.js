51d3c991c4df14cea6898dc7ac51a31dd2210d77
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-12-11 23:09:57 +0500

    account-personal-edit page the end

--- js/main.js
+++ js/main.js
@@ -20,6 +20,41 @@ document.querySelectorAll('input[name="gender"]').forEach(radio => {
 });
 // custom-radio
 
+// password
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+const validateButton = document.getElementById("validateButton");
+const errorMessage = document.querySelector(".error-message");
+const clearIcon = document.getElementById("clearIcon");
+
+// Validate passwords
+validateButton.addEventListener("click", () => {
+  // Reset styles
+  resetErrors();
+
+  // Check if passwords match
+  if (password1.value !== password2.value || password1.value === "" || password2.value === "") {
+    password2.classList.add("error");
+    errorMessage.style.display = "flex";
+    clearIcon.style.display = "inline";
+  }
+});
+
+// Clear input and reset errors
+clearIcon.addEventListener("click", () => {
+  password2.value = "";
+  resetErrors();
+});
+
+// Reset error styles
+function resetErrors() {
+  password2.classList.remove("error");
+  errorMessage.style.display = "none";
+  clearIcon.style.display = "none";
+}
+
+// password
+
 // account_modal
 let account_modal_bg = document.querySelectorAll('.account_modal_bg');
 let account_modal = document.querySelectorAll('.account_modal');
